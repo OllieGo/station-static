88305e31146c4733d33b76562f7d960506383445
高万里 <gaowanli@zhujia360.com>
2022-06-06 16:19:36 +0800

    init

---
+++ pages/hot/hot.vue
@@ -0,0 +1,25 @@
+<template>
+	<view class="hot-container">
+		<image class="logo" src="@/static/images/olliego.jpg" mode="aspectFill" />
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			};
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.hot-container {
+		background-color: #ffffff;
+		.logo {
+			width: 100%;
+			height: 150px;
+		}
+	}
+</style>
---
+++ unpackage/dist/dev/mp-weixin/pages/hot/hot.js
@@ -0,0 +1,15 @@
+"use strict";
+var common_vendor = require("../../common/vendor.js");
+var _imports_0 = "/static/images/olliego.jpg";
+const _sfc_main = {
+  data() {
+    return {};
+  }
+};
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return {
+    a: _imports_0
+  };
+}
+var MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__scopeId", "data-v-41251cbc"], ["__file", "D:/coding/ollieGoBlog/pages/hot/hot.vue"]]);
+wx.createPage(MiniProgramPage);
