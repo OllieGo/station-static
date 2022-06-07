e54916a31a7ea3b6d5871f401c7965d048f1236d
olliego <olliego@126.com>
2022-06-07 09:03:52 +0800

    hot view

--- pages/hot/hot.vue
+++ pages/hot/hot.vue
@@ -1,6 +1,10 @@
 <template>
 	<view class="hot-container">
 		<image class="logo" src="@/static/images/olliego.jpg" mode="aspectFill" />
+		<view class="search-box">
+			<my-search></my-search>
+		</view>
+		
 	</view>
 </template>
 
@@ -20,6 +24,10 @@
 		.logo {
 			width: 100%;
 			height: 150px;
+		}
+		.search-box {
+			padding: 0 16px;
+			margin-bottom: 8px;
 		}
 	}
 </style>
